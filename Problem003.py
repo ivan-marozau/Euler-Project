7934629f190b9396ffd8a580849547f9b939c88a
Ivan Marozau <ivan-marozau@users.noreply.github.com>
2016-11-03 17:50:57 +0300

    Update Problem003.py

--- Problem003.py
+++ Problem003.py
@@ -3,30 +3,66 @@
 # What is the largest prime factor of the number 600851475143 ?
 
 import time
-start = time.clock()
+
+def c_range(n):
+    cur = 5
+    incr = 2
+    while cur < n+1:
+        yield cur
+        cur += incr
+        incr = 6 - incr
+
+def sieve(end):
+    primes_lst = [2, 3]
+    sieve_lst = [True] * (end+1)
+    for num in c_range(end):
+        if sieve_lst[num]:
+            primes_lst.append(num)
+            for multiple in range(num**2, end+1, num):
+                sieve_lst[multiple] = False
+    return primes_lst
+
+def lpf_sieve(n):
+	if n == 1: return None
+	while n%2 == 0:
+		n = n//2
+	if n == 1: return 2
+	i = 0
+	lst = sieve(int(n**0.5)+1)
+	while i < len(lst) and lst[i] < n:
+		if n%lst[i] == 0:
+			n = n//lst[i]
+		else:
+			i += 1
+		if i == len(lst):
+			return n
+	return lst[i]
 
 def lpf(n):
-	if n%2 == 0:
-		lastFactor = 2
-		n = n//2
-		while n%2 == 0:
-	 		n=n//2
-	else:
-		lastFactor=1
-		factor=3
-	while n>1 and factor**2 <= n:
-	 	if n%factor==0:
-	 		n=n//factor
-	 		lastFactor=factor
-	 		while n%factor==0:
-	 			n=n//factor
-	 	factor=factor+2
-	if n==1:
-		return lastFactor
-	else:
-		return n
+	if n == 1: return None
+	while n%2 == 0:
+		n //= 2
+	if n == 1: return 2
+	i = 3
+	while i <= n:
+		if n%i == 0:
+			n = n//i
+		else:
+			i += 2
+		if i > 10000:
+			return lpf_sieve(n)
+	return i
 	
-print (lpf(600851475143))
+initial_number = int(input('Enter the number:\n'))
+n = initial_number
 
+start = time.clock()
+lpf = lpf(n)
 end = time.clock()
-print ("Running time: %s seconds" % (end - start))
+
+if initial_number == lpf:
+	print ("%s is prime." % lpf)
+else:
+	print ("The largest prime factor of %s is %s." % (initial_number, lpf))
+	
+print ("Running time: %s ms" % round((end - start)*10**3))
